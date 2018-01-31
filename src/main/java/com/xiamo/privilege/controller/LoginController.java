d2c32a3191f51c4c3fb07cbe28655e1dd96c8923
chenglt <593482670@qq.com>
2018-01-31 22:44:30 +0800

    时候

--- src/main/java/com/xiamo/privilege/controller/LoginController.java
+++ src/main/java/com/xiamo/privilege/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.xiamo.privilege.controller;
 
+import com.xiamo.common.po.ServiceException;
 import com.xiamo.common.vo.AjaxResultPo;
 import com.xiamo.privilege.constant.UserStatus;
 import com.xiamo.privilege.po.UserPo;
@@ -9,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * <dd>Company:  程立涛</dd>
  * <dd>CreateDate: 2017/12/25 0025</dd>
  * </dl>
- *  登陆接口
+ * 登陆接口
+ *
  * @author chenglitao
  */
 @Controller
@@ -34,31 +35,35 @@ public class LoginController {
 
     @ResponseBody
     @RequestMapping("/index")
-    public String index(String loginName, String password, Model model) {
+    public AjaxResultPo index(String loginName, String password) {
         logger.info("进入LoginController.index方法，loginName={}，password={}", loginName, password);
-        AjaxResultPo res = new AjaxResultPo(true,"操作成功");
+        AjaxResultPo res = new AjaxResultPo(true, "操作成功");
         try {
 
             String MD5Password = DigestUtils.md5Hex(password.trim());
             UserPo userPo = userServiceImpl.loginByName(loginName);
 
             if (userPo == null) {
-                model.addAttribute("errMsg", "用户名或密码错误!");
+                throw new ServiceException("用户名或密码错误!");
+
             }
 
             if (!userPo.getPassword().equals(MD5Password)) {
-                model.addAttribute("errMsg", "用户名或密码错误!");
+                throw new ServiceException("用户名或密码错误!");
             }
 
             if (UserStatus.STOP.value.equals(userPo.getUserType())) {
+                throw new ServiceException("用户已注销!");
 
-                model.addAttribute("errMsg", "用户已注销!");
             }
-            return "index";
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            res.setSuccess(false);
+            res.setMsg(e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
             res.setMessage("查询用户信息失败");
         }
-        return "login";
+        return res;
     }
 }
